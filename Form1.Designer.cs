e815b179749dd895e67e0d3721b2aa60957dc770
dspyun <dspyun@naver.com>
2024-11-05 08:45:55 +0900

    Update Form1.Designer.cs

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,8 +36,9 @@ namespace SerialTerminal
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button6 = new System.Windows.Forms.Button();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
@@ -51,10 +52,10 @@ namespace SerialTerminal
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(125, 10);
+            this.button1.Location = new System.Drawing.Point(116, 8);
             this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(118, 27);
+            this.button1.Size = new System.Drawing.Size(100, 30);
             this.button1.TabIndex = 1;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
@@ -62,10 +63,10 @@ namespace SerialTerminal
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(249, 9);
+            this.button2.Location = new System.Drawing.Point(222, 8);
             this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(118, 27);
+            this.button2.Size = new System.Drawing.Size(100, 30);
             this.button2.TabIndex = 2;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
@@ -74,17 +75,17 @@ namespace SerialTerminal
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(3, 10);
+            this.comboBox1.Location = new System.Drawing.Point(3, 11);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(116, 26);
+            this.comboBox1.Size = new System.Drawing.Size(107, 26);
             this.comboBox1.TabIndex = 3;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(373, 9);
+            this.button3.Location = new System.Drawing.Point(328, 8);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(116, 27);
+            this.button3.Size = new System.Drawing.Size(100, 30);
             this.button3.TabIndex = 4;
             this.button3.Text = "SaveFile";
             this.button3.UseVisualStyleBackColor = true;
@@ -92,28 +93,19 @@ namespace SerialTerminal
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(495, 9);
+            this.button4.Location = new System.Drawing.Point(434, 8);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(112, 27);
+            this.button4.Size = new System.Drawing.Size(100, 30);
             this.button4.TabIndex = 5;
             this.button4.Text = "Buf Clear";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(613, 9);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(109, 26);
-            this.button5.TabIndex = 7;
-            this.button5.Text = "gps_info";
-            this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.button5_Click);
-            // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.comboBox2);
+            this.panel1.Controls.Add(this.button6);
             this.panel1.Controls.Add(this.comboBox1);
-            this.panel1.Controls.Add(this.button5);
             this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.button2);
             this.panel1.Controls.Add(this.button4);
@@ -123,6 +115,25 @@ namespace SerialTerminal
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(1030, 711);
             this.panel1.TabIndex = 8;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(646, 11);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(290, 26);
+            this.comboBox2.TabIndex = 9;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(942, 8);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(80, 30);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Send";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // tableLayoutPanel1
             // 
@@ -166,11 +177,10 @@ namespace SerialTerminal
             this.richTextBox1.Size = new System.Drawing.Size(1027, 489);
             this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = "";
-            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);       
+            this.richTextBox1.VScroll += new System.EventHandler(this.RichTextBox1_VScroll);
             this.richTextBox1.Click += new System.EventHandler(this.RichTextBox1_Click);
-			this.richTextBox1.VScroll += new System.EventHandler(this.RichTextBox1_VScroll);
-
-			// 
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
             // richTextBox2
             // 
             this.richTextBox2.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -212,12 +222,14 @@ namespace SerialTerminal
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.SplitContainer splitContainer1;
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.ComboBox comboBox2;
         // private System.Windows.Forms.TextBox textBox1;
     }
 }
+
